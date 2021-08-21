0e837bfe6874a8bc7b8688838d1bfe4cc006b3a3
seolyu <seolyu.tjfdb@gmail.com>
2021-08-21 18:13:42 +0900

    CSS & JS: Work

---
+++ main.js
@@ -0,0 +1,40 @@
+// Projects
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector(".category__btn.selected");
+    active.classList.remove("selected");
+    const target =
+        e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+    target.classList.add("selected");
+
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+
+        // for(let project of projects) {
+
+        // }
+
+        // let project;
+        // for(let i = 0; i < projects.length; i++) {
+        //   project = projects[i];
+        // }
+
+        projectContainer.classList.remove("anim-out");
+    }, 300);
+});
