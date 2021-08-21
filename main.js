def73a5aeedc9b487519c1988c4f34bc779f2335
seolyu <seolyu.tjfdb@gmail.com>
2021-08-21 20:07:37 +0900

    JS: Navbar

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+"use strict";
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight: ${navbarHeight}`);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add("navbar--dark");
+    } else {
+        navbar.classList.remove("navbar--dark");
+    }
+});
+
 // Projects
 const workBtnContainer = document.querySelector(".work__categories");
 const projectContainer = document.querySelector(".work__projects");
