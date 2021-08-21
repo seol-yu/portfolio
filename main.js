0fbc2bbef73f67a79ea28b808c20874906371b26
seolyu <seolyu.tjfdb@gmail.com>
2021-08-21 21:23:15 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -22,9 +22,21 @@ navbarMenu.addEventListener("click", (event) => {
     if (link == null) {
         return;
     }
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({ behavior: "smooth" });
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+    scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
 
 // Projects
 const workBtnContainer = document.querySelector(".work__categories");
